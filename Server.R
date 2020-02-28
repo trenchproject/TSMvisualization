5fa6f72839b21eaa78d3740981e2adeb3b459536
lbuckley <ys634@uw.edu>
2020-02-27 16:15:51 -0800

    shiny improved, and LFS large storage didn't work

--- Server.R
+++ Server.R
@@ -3,44 +3,54 @@ names(month) <- c("January","February","March","April","May","June","July","Augu
 scenarios <- c("Normal","+1.5 °C","+2 °C")
 hours <- c("12 AM","01 AM","02 AM","03 AM","04 AM","05 AM","06 AM","07 AM","08 AM","09 AM","10 AM","11 AM","12 PM","01 PM","02 PM","03 PM","04 PM","05 PM","06 PM","07 PM","08 PM","09 PM", "10 PM","11 PM")
 
-#setwd("\\Volumes\\GoogleDrive\\Shared Drives\\TrEnCh\\TSMVisualization\\TSMdfs")
 #setwd("/Volumes/GoogleDrive/Shared Drives/TrEnCh/TSMVisualization/TSMdfs")
 
-my_db <- src_sqlite("TSM data", create = FALSE)
+#lizards_tpc <- fread("lizards_tpc.csv")
+
 
 shinyServer <- function(input, output) {
   
-  # Method 1: Rda file
+  #Method 1: Rda file
   # data_by_org <- reactive({
-  #   org <- input$select_species
+  #   org <- input$species
   #   spdata <- paste("TSMdfs\\",gsub(" ","_",org),"_combined.Rda", sep= "")
   #   load(spdata)
-  #   combined
+  #   df <- combined
+  #   df$Month <- factor(df$Month, levels = names(month))
+  #   df$Hour <- factor(df$Hour, levels = hours)
+  #   df$Scenario <- factor(df$Scenario, levels = scenarios)
+  #   df
   # })
-   
   
   #Method 2: fread from csv
+  # data_by_org <- eventReactive( input$run,{
+  # 
+  #   org <- input$species_1
+  #   filename <- paste("Data\\",gsub(" ","_",org),"_combined.csv", sep= "")
+  #   df <- data.table::fread(filename)
+  #   df$Month <- factor(df$Month, levels = names(month))
+  #   df$Hour <- factor(df$Hour, levels = hours)
+  #   df$Scenario <- factor(df$Scenario, levels = scenarios)
+  #   df
+  # }, ignoreNULL = FALSE
+  # )
+  
+  #Method 3: RDS file
   data_by_org <- eventReactive( input$run,{
 
-    org <- input$select_species
-    filename <- paste("Data\\",gsub(" ","_",org),"_combined.csv", sep= "")
-    df <- data.table::fread(filename)
+    org <- input$species_1
+    filename <- paste("Data\\",gsub(" ","_",org),"_combined.rds", sep= "")
+    df <- readRDS(filename)
+    df$Month <- factor(df$Month, levels = names(month))
+    df$Hour <- factor(df$Hour, levels = hours)
+    df$Scenario <- factor(df$Scenario, levels = scenarios)
+    df
   }, ignoreNULL = FALSE
   )
   
-  
-  # Method 3: RDS file
-  # data_by_org <- reactive({
-  #   
-  #   org <- input$select_species
-  #   filename <- paste("TSMdfs\\",gsub(" ","_",org),"_combined.rds", sep= "")
-  #   df2 <- readRDS(filename)
-  #   df2
-  # })
-  
   # Method 4: Database using dbWriteTable(), dbReadTable()
   # data_by_org <- reactive({
-  #   org <- input$select_species
+  #   org <- input$species
   #   path <- sub(" ","_",org)
   #   a <- dbReadTable(db, path)
   #   a[a$Shade=="Exposed\r",]$Shade <- "Exposed"
@@ -50,44 +60,42 @@ shinyServer <- function(input, output) {
   
   # # Method 5: database using src_sqlite
   # dataInput <- eventReactive( input$run, {
-  #   mm <- names(month[as.numeric(input$select_month)])
-  #   org <- input$select_species
+  #   mm <- names(month[as.numeric(input$month)])
+  #   org <- input$species
   #   tbl(my_db, paste(sub(" ","_",org), "_combined", sep = "")) %>% 
-  #     filter(Hour %in% local(input$select_hour) & Month %in% mm & Scenario %in% local(input$select_scenario) & Shade %in% local(input$select_shade)) %>%
+  #     filter(Hour %in% local(input$hour) & Month %in% mm & Scenario %in% local(input$scenario) & Shade %in% local(input$shade)) %>%
   #     tbl_df()
   # }, ignoreNULL = FALSE
   # )
   
   
-  # data_by_org <- tbl(my_db, paste(sub(" ","_",org), "_combined", sep = "")) %>%
-  #   filter(Hour %in% "01 PM" & Month %in% "January" & Scenario %in% "Normal" & Shade %in% "Covered") %>% 
-  #   tbl_df()
-  
   # data_by_org_2 <- reactive({
-  #   org <- input$select_species_2
+  #   org <- input$species_2
   #   spdata <- paste("TSMdfs\\",gsub(" ","_",org),"_combined.Rda", sep= "")
   #   load(spdata)
   #   combined
   # })
   
   # data_by_org_2 <- reactive({
-  #   org_2 <- input$select_species_2
-  #   filename <- paste("TSMdfs\\",gsub(" ","_",org_2),"_combined.csv", sep= "")
+  #   org_2 <- input$species_2
+  #   filename <- paste("Data\\",gsub(" ","_",org_2),"_combined.csv", sep= "")
   #   df <- data.table::fread(filename)
   #   levels(df$Hour) <- hours
   #   levels(df$Month) <- month
   #   df
   # })
   
-  # data_by_org_2 <- reactive({
-  #   org_2 <- input$select_species_2
-  #   filename <- paste("TSMdfs\\",gsub(" ","_",org),"_combined.rds", sep= "")
-  #   df2 <- readRDS(filename)
-  #   df2
-  # })
+  data_by_org_2 <- reactive({
+    org_2 <- input$species_2
+    filename <- paste("Data\\",gsub(" ","_",org_2),"_combined.rds", sep= "")
+    df2 <- readRDS(filename)
+    levels(df2$Hour) <- hours
+    levels(df2$Month) <- month
+    df2
+  })
   
   # data_by_org_2 <- reactive({
-  #   org <- input$select_species_2
+  #   org <- input$species_2
   #   path <- sub(" ","_",org)
   #   a <- dbReadTable(db, path)
   #   a[a$Shade=="Exposed\r",]$Shade <- "Exposed"
@@ -96,38 +104,39 @@ shinyServer <- function(input, output) {
   # })
   
   
-  dataInput_2 <- reactive({
-    mm <- names(month[as.numeric(input$select_month_2)])
-    org <- input$select_species_2
-    df <- tbl(my_db, paste(sub(" ","_",org), "_combined", sep = "")) %>% 
-            filter(Month %in% mm) %>%
-            tbl_df()
-    levels(df$Hour) <- hours
-    levels(df$Month) <- month
-    df
-  })
+  # dataInput_2 <- reactive({
+  #   mm <- names(month[as.numeric(input$month_2)])
+  #   org <- input$species_2
+  #   df <- tbl(my_db, paste(sub(" ","_",org), "_combined", sep = "")) %>% 
+  #           filter(Month %in% mm) %>%
+  #           tbl_df()
+  #   levels(df$Hour) <- hours
+  #   levels(df$Month) <- month
+  #   df
+  # })
   
 
   dataInput <- eventReactive( input$run,{
-    data_by_org() %>% filter(Hour %in% input$select_hour & Month %in% names(month[as.numeric(input$select_month)]) & Scenario %in% input$select_scenario & Shade %in% input$select_shade)
+    data_by_org() %>% filter(Hour %in% input$hour & Month %in% names(month[as.numeric(input$month)]) & Scenario %in% input$scenario & Shade %in% input$shade)
   }, ignoreNULL = FALSE
   )
   
-  # dataInput_2 <- reactive({
-  #   data_by_org_2() %>% filter(Month %in% names(month[as.numeric(input$select_month_2)]))
-  # })
   
+  dataInput_2 <- reactive({
+    data_by_org_2() %>% filter(Month %in% names(month[as.numeric(input$month_2)]))
+  })
+
   
   dataTPC <- reactive({
-    org <- input$select_species_tpc
+    org <- input$species_tpc
     filename <- paste("Data\\",gsub(" ","_",org),"_combined.csv", sep= "")
     df <- data.table::fread(filename)
-    df <- df %>% filter(Hour %in% input$select_hour_tpc & Month %in% names(month[as.numeric(input$select_month_tpc)]) & Scenario %in% input$select_scenario_tpc & Shade %in% input$select_shade_tpc)
-    41.6 - mean(df$Tsm)
+    df <- df %>% filter(Hour %in% input$hour_tpc & Month %in% names(month[as.numeric(input$month_tpc)]) & Scenario %in% input$scenario_tpc & Shade %in% input$shade_tpc)
+    lizards_tpc[lizards_tpc$Binomial == input$species_tpc, Tmax] - mean(df$Tsm)
   })
   
   x_variable <- eventReactive( input$run, {
-    switch(input$columns, Month = "factor(Month, levels = names(month))", Hour = "Hour", Scenario = "Scenario", Shade = "Shade")
+    switch(input$columns, Month = "Month", Hour = "Hour", Scenario = "Scenario", Shade = "Shade")
   }, ignoreNULL = FALSE
   )
   
@@ -137,10 +146,10 @@ shinyServer <- function(input, output) {
   )
   
   title <- eventReactive( input$run, {
-    title_m <- paste("|", names(month[as.numeric(input$select_month)]))
-    title_h <- paste("|", input$select_hour)
-    title_sc <- paste("|", input$select_scenario)
-    title_sh <- paste("|", input$select_shade)
+    title_m <- paste("| ", names(month[as.numeric(input$month)]))
+    title_h <- paste("| ", input$hour)
+    title_sc <- paste("| ", input$scenario)
+    title_sh <- paste("| ", input$shade)
     
     if (as.character(x_variable()) == "Month" | as.character(y_variable()) == "Month") {
       title_m <- ""
@@ -154,13 +163,13 @@ shinyServer <- function(input, output) {
     if (as.character(x_variable()) == "Shade" | as.character(y_variable()) == "Shade") {
       title_sh <- ""
     }     
-    paste(input$select_species, title_m, title_h, title_sc, title_sh)
+    paste(input$species_1, title_m, title_h, title_sc, title_sh)
   }, ignoreNULL = FALSE
   )
   
   output$mymap <- renderLeaflet({
     leaflet() %>%
-      addTiles() %>%
+      addProviderTiles(providers$CartoDB.Positron) %>%
       setView(lng = (max(dataInput()$x) + min(dataInput()$x))/2,  lat = (max(dataInput()$y) + min(dataInput()$y))/2, zoom = 3) %>%
       addRectangles(lng1 = min(dataInput()$x), lng2 = max(dataInput()$x), lat1 = min(dataInput()$y), lat2 = max(dataInput()$y))
       
@@ -174,47 +183,57 @@ shinyServer <- function(input, output) {
   output$plot1 <- renderPlot({
     
     facet_formula <- as.formula(paste(x_variable(), "~", y_variable()))
+    #facet_formula <- as.formula(paste("factor(\"Month\", levels = names(month)) ~", y_variable()))
     
     ggplot() +
       borders(fill="grey",colour="black") +
-      ggtitle(title()) + xlab("Longitude") + ylab("Latitude") +
+      ggtitle(title()) + xlab("Longitude (°)") + ylab("Latitude (°)") +
       geom_raster(data=dataInput(), aes(x = x, y = y, fill = Tsm)) + scale_fill_gradient(low = "red", high = "blue", name = "Thermal Safety \nMargin (°C)") +
-      facet_grid(facet_formula) +
-      coord_quickmap(xlim = c(min(dataInput()$x), max(dataInput()$x)), ylim = c(min(dataInput()$y), max(dataInput()$y)),expand = TRUE) +
+      facet_grid(facet_formula) + coord_quickmap(xlim = c(min(dataInput()$x), max(dataInput()$x)), ylim = c(min(dataInput()$y), max(dataInput()$y)),expand = TRUE) +
       theme_bw( ) + theme(strip.text = element_text(size = 12)) +
-      theme(plot.title = element_text(size = 16), axis.text = element_text(size = 12), axis.title = element_text(size = 14), legend.text = element_text(size = 12), legend.title = element_text(size = 12))
-  }, height = 600, width = 900)
+      theme(plot.title = element_text(size = 18), axis.text = element_text(size = 12), axis.title = element_text(size = 14), legend.text = element_text(size = 12),
+            legend.title = element_text(size = 12))
+    
+  }, height = 650, width = 650)
     
   
 
   output$TPC <- renderPlot({
-    lizards_curve <- subset(lizardsdf, !is.na(lizardsdf$tmin) & !is.na(lizardsdf$Topt))
-    lizards_curve <- lizards_curve[lizards_curve$Binomial == input$select_species_tpc, ]
-    curve_ex1 <- TPC(0:50, as.numeric(as.character(lizards_curve[1,]$Topt)), as.numeric(as.character(lizards_curve[1,]$tmin)), as.numeric(as.character(lizards_curve[1,]$Tmax)))
+    lizards_tpc <- lizards_tpc[lizards_tpc$Binomial == input$species_tpc, ]
+    curve <- TPC(0:pmax(50, dataTPC()), lizards_tpc$Topt, as.numeric(as.character(lizards_tpc$tmin)), lizards_tpc$Tmax)
     
-    ggplot(data = NULL, aes(x = c(0:50), y = curve_ex1)) + geom_line(col = "steelblue", size = 1.2) + xlab("Temperature (°C)") + ylab("Performance") +
-      geom_vline(xintercept = dataTPC(), size = 1.2) +
-      theme(plot.title = element_text(size = 16), axis.text = element_text(size = 12), axis.title = element_text(size = 14), legend.text = element_text(size = 12), legend.title = element_text(size = 12))
-    
+    ggplot() + geom_line(data = NULL, aes(x = c(0:pmax(50, dataTPC())), y = curve), col = "steelblue", size = 1.2) + xlab("Temperature (°C)") + ylab("Performance") +
+      geom_segment(aes(x = lizards_tpc$Tmax - 5, y = 0.1, xend = lizards_tpc$Tmax, yend = 0), size = 1) + geom_text(aes(x = lizards_tpc$Tmax - 6, y = 0.14, label = "Tmax"), size = 5) +
+      geom_vline(xintercept = dataTPC(), size = 1.2) + geom_text(aes(x = dataTPC() -1, label = "Operative temperature", y = 0.45), size = 5, angle = 90) + theme_classic( ) +
+      geom_rect(aes(xmin = lizards_tpc$Tmax, xmax = Inf, ymin = -Inf, ymax = Inf), alpha = 0.7, fill = "red") +
+      scale_x_continuous(limits = c(0,pmax(50, dataTPC())+2), expand = c(0,0)) + scale_y_continuous(limits = c(0, 1.05), expand = c(0,0)) +
+      theme(axis.text = element_text(size = 12), axis.title = element_text(size = 15))
+      
   })
   
   output$plot2 <- renderPlot({
     
-    if (input$select_facet == "Shade") {
-      ggplot(dataInput_2(), aes(x=Tsm, y= Hour)) + xlab("Thermal Safety Margin (°C)") + 
-        geom_density_ridges2(aes(fill=Scenario), rel_min_height = 0.01, scale=2, alpha=0.5) + ggtitle(paste(input$select_species_2,"|", names(month[as.numeric(input$select_month_2)]))) + 
+    if (input$facet == "Shade") {
+      ggplot(dataInput_2(), aes(x=Tsm, y= Hour)) + 
+        xlab("Thermal Safety Margin (°C)") + geom_density_ridges2(aes(fill=Scenario), rel_min_height = 0.01, scale=2, alpha=0.5) + 
+        ggtitle(paste(input$species_2,"|", names(month[as.numeric(input$month_2)]))) + 
         scale_y_discrete(limits = rev(levels(dataInput_2()$Hour)))  + theme_bw() + facet_wrap(~Shade) + theme(strip.text = element_text(size = 12)) +
-        theme(plot.title = element_text(size = 16), axis.text = element_text(size = 12), axis.title = element_text(size = 14), legend.text = element_text(size = 12), legend.title = element_text(size = 12))
+        theme(plot.title = element_text(size = 18), axis.text = element_text(size = 12), axis.title = element_text(size = 14), legend.text = element_text(size = 12),
+              legend.title = element_text(size = 12))
       
     } else {
-      ggplot(dataInput_2(),aes(x=Tsm, y= Hour)) + xlab("Thermal Safety Margin (°C)") + 
-        geom_density_ridges2(aes(fill=Shade),rel_min_height = 0.01 ,scale=2,alpha=0.5) + ggtitle(paste(input$select_species_2,"|", names(month[as.numeric(input$select_month_2)]))) + 
-        scale_y_discrete(limits = rev(levels(dataInput_2()$Hour)))  + theme_bw() + facet_wrap(~factor(Scenario, levels = scenarios)) + scale_fill_manual(values = c("blue","red")) +
-        theme(strip.text = element_text(size = 12)) +
-        theme(plot.title = element_text(size = 16), axis.text = element_text(size = 12), axis.title = element_text(size = 14), legend.text = element_text(size = 12), legend.title = element_text(size = 12))
+      ggplot(dataInput_2(),aes(x=Tsm, y= Hour)) + 
+        xlab("Thermal Safety Margin (°C)") + geom_density_ridges2(aes(fill=Shade),rel_min_height = 0.01 ,scale=2,alpha=0.5) + 
+        ggtitle(paste(input$species_2,"|", names(month[as.numeric(input$month_2)]))) + 
+        scale_y_discrete(limits = rev(levels(dataInput_2()$Hour)))  + theme_bw() + facet_wrap(~factor(Scenario, levels = scenarios)) + 
+        scale_fill_manual(values = c("blue","red")) + theme(strip.text = element_text(size = 12)) +
+        theme(plot.title = element_text(size = 18), axis.text = element_text(size = 12), axis.title = element_text(size = 14), legend.text = element_text(size = 12),
+              legend.title = element_text(size = 12))
     }
     
-    
-  }, height = 600, width = 800)  
+  }, height = 600, width = 600)  
   
+  output$text_data <- renderText({
+    
+  })
 }
--- ui.R
+++ ui.R
@@ -1,106 +1,135 @@
 pkgs <- c('pdftools','tidyverse','taxize', 'raster','rgdal','plotly','reshape2','taxize', 'magrittr', 'TrenchR', 'ggplot2','ggridges','plotly', 'data.table', 'leaflet', 'shinyjs')
 lapply(pkgs, library, character.only = TRUE)
-#setwd("C:\\Users\\Bryan\\Google Drive\\TSMVisualization\\")
-#setwd("C:\\Users\\lbuckley\\My Documents\\TSMviz")
+
 #setwd("/Volumes/GoogleDrive/Shared Drives/TrEnCh/TSMVisualization/")
 
 month <- c(1:12)
 names(month) <- c("January","February","March","April","May","June","July","August","September","October","November","December")
 scenarios <- c("Normal","+1.5 °C","+2 °C")
 hours <- c("12 AM","01 AM","02 AM","03 AM","04 AM","05 AM","06 AM","07 AM","08 AM","09 AM","10 AM","11 AM","12 PM","01 PM","02 PM","03 PM","04 PM","05 PM","06 PM","07 PM","08 PM","09 PM", "10 PM","11 PM")
-org <- c("Takydromus sexlineatus","Coleonyx brevis","Holbrookia maculata","Lepidophyma flavimaculatum", "Psammodromus algirus", "Sceloporus undulatus", "Cophosaurus texanus", "Petrosaurus mearnsi","Platysaurus intermedius","Psammodromus hispanicus","Sceloporus magister","Tiliqua rugosa","Urosaurus ornatus")
+org <- c("Takydromus sexlineatus","Coleonyx brevis","Psammodromus hispanicus", "Holbrookia maculata","Lepidophyma flavimaculatum", "Psammodromus algirus", "Sceloporus undulatus", "Cophosaurus texanus", "Petrosaurus mearnsi","Platysaurus intermedius","Psammodromus hispanicus","Sceloporus magister","Tiliqua rugosa","Urosaurus ornatus")
 variables <- c("Month", "Hour", "Scenario", "Shade")
 
-lizards_curve <- subset(lizardsdf, !is.na(lizardsdf$tmin) & !is.na(lizardsdf$Topt))
+#lizards_tpc <- fread("lizards_tpc.csv")
 
 
 
 shinyUI <- fluidPage (
   useShinyjs(),
-  titlePanel("Thermal Safety Margin"),
+  titlePanel("Climate Change and Lizards"),
   
-  mainPanel(
-    h3("Map (only the first seven species work)"),
-    
-    fluidRow(
-      column(8, selectInput("select_species", label = "Species", choices = org, selected = "Coleonyx brevis")),
+  tabsetPanel(type = "tabs", 
+    tabPanel("Distribution Map",
+      sidebarLayout(
+        sidebarPanel(
+          h3("Distribution map"),
+          p("Select a species and hit \"Run\" to take a look at their thermal safety margins depending on various conditions within their habitat."),
+          selectInput("species_1", label = "Species", choices = org, selected = "Coleonyx brevis"),
+          
+          fluidRow(
+            column(6,radioButtons("rows", label = "Horizontal facets", choices = variables)),
+            column(6,radioButtons("columns", label = "Vertical facets", choices = variables, selected = "Hour"))
+          ),
+          
+          selectInput("month", label = "Month", choices = month, multiple = TRUE, selected = 1),
+          selectInput("hour", label = "Hour", choices = hours, multiple = TRUE, selected = "01 PM"),
+          
+          fluidRow(
+            column(6, checkboxGroupInput("scenario", label = "Scenario", choices = scenarios, selected = "Normal")),
+            column(6, checkboxGroupInput("shade", label = "Shade", choices = c("Covered", "Exposed"), selected = "Covered"))
+          ),  
+          
+          fluidRow(
+            column(2, actionButton("map_onoff", "Show/Hide map")),
+            column(2, offset = 4, actionButton("run", "Run"))
+            
+          ),
+        ),
+        mainPanel(
+        
+          fluidRow(
+            column(12, plotOutput("plot1", width = "100%"))
+          ), 
+          
+          br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(),
+          
+        
+          fluidRow(
+            column(8, leafletOutput("mymap"))
+          ),
+          
+          br(),
+        )
+      )
     ),
     
-    hr(),
+    tabPanel("Thermal Performance Curve",
+      sidebarLayout(
+        sidebarPanel(
     
-    fluidRow(
-      column(4, radioButtons("rows", label = "Horizontal facets", choices = variables, inline = TRUE)),
-      column(4, offset = 1, radioButtons("columns", label = "Vertical facets", choices = variables, selected = "Hour", inline = TRUE))
-      
-    ),
-
-    fluidRow(
-      column(5, selectInput("select_month", label = "Month", choices = month, multiple = TRUE, selected = 1)),
-      column(5, selectInput("select_hour", label = "Hour", choices = hours, multiple = TRUE, selected = "01 PM"))
-      
-    ),
-    
-    
-    fluidRow(
-      column(5, checkboxGroupInput("select_scenario", label = "Scenario", choices = scenarios, selected = "Normal")),
-      column(5, checkboxGroupInput("select_shade", label = "Shade", choices = c("Covered", "Exposed"), selected = "Covered")),
-      column(2, actionButton("run", "Run"))
-      
-    ),
-    
-    br(),
-    
-    fluidRow(
-      column(8, offset = 1, leafletOutput("mymap")),
-      column(2, offset = 1, actionButton("map_onoff", "Show/Hide map"))
+          h3("Thermal performance curve"),
+          p("Select a species to look at their thermal performance curve and how varying environmental conditions affects their performance."),
+          selectInput("species_tpc", label = "Species", choices = lizards_tpc$Binomial), 
+          selectInput("month_tpc", label = "Month", choices = month, selected = 1),
+          selectInput("hour_tpc", label = "Hour", choices = hours, selected = "01 PM"),
+          
+          
+          fluidRow(
+            column(6, radioButtons("scenario_tpc", label = "Scenario", choices = scenarios, selected = "Normal")),
+            column(6, radioButtons("shade_tpc", label = "Shade", choices = c("Covered", "Exposed"), selected = "Covered")),
+          )
+        ),
+        
+        mainPanel(
+          br(),
+          plotOutput("TPC")
+        )
+      ),
+      br(),
     ),
     
-    br(),
-    
-    fluidRow(
-      column(12, plotOutput("plot1", width = "100%"))
-    ), 
-    
-    br(), br(), br(), br(), br(), br(), br(), br(), br(), br(),
-  
-    
-    hr(),
-    
-    h3("Plot"),
-    
-    fluidRow(
-      column(5, selectInput("select_month_tpc", label = "Month", choices = month, selected = 1)),
-      column(5, selectInput("select_hour_tpc", label = "Hour", choices = hours, selected = "01 PM"))
+    tabPanel("Plot", 
+      sidebarLayout(
+        sidebarPanel(
+          h3("Plot"),
+          
+          selectInput("species_2", label = "Species", choices = org, selected = "Coleonyx brevis"),
+          
+          radioButtons("facet", label = "Facet", choices = c("Shade", "Scenario"), inline = TRUE),
+        
+          selectInput("month_2", label = "Month", choices = month)
+        ),
+        
+        mainPanel(
+          plotOutput("plot2", width = "100%")
+          
+        )
+      )
     ),
     
-    
-    fluidRow(
-      column(5, radioButtons("select_scenario_tpc", label = "Scenario", choices = scenarios, selected = "Normal")),
-      column(5, radioButtons("select_shade_tpc", label = "Shade", choices = c("Covered", "Exposed"), selected = "Covered")),
-    ),
-    
-    fluidRow(
-      column(6, selectInput("select_species_tpc", label = "Species", choices = lizards_curve$Binomial))
-    ), 
-    
-    fluidRow(
-      column(12, plotOutput("TPC"))
-    ),
-    
-    fluidRow(
-      column(8, selectInput("select_species_2", label = "Species", choices = org, selected = "Coleonyx brevis")),
-    ),
-    
-    fluidRow(
-      column(5, radioButtons("select_facet", label = "Facet", choices = c("Shade", "Scenario")))
-    ),
-    
-    fluidRow(
-      column(5, selectInput("select_month_2", label = "Month", choices = month)),
-    ),
-    
-    fluidRow(
-      column(12, plotOutput("plot2", width = "100%"))
-    ), 
+    tabPanel("Data", 
+      sidebarLayout(
+        sidebarPanel(
+          h3("Data"),
+          
+          selectInput("species_data", label = "Species", choices = org, selected = "Coleonyx brevis"),
+          
+          selectInput("month_data", label = "Month", choices = month),
+          selectInput("hour_data", label = "Hour", choices = hours, selected = "01 PM"),
+          
+          
+          fluidRow(
+            column(6, radioButtons("scenario_data", label = "Scenario", choices = scenarios, selected = "Normal")),
+            column(6, radioButtons("shade_data", label = "Shade", choices = c("Covered", "Exposed"), selected = "Covered")),
+          )
+        ),
+          
+        
+        
+        mainPanel(
+          textOutput("text_data")
+        )
+      )
+    )
   )
 )
