b8d907a113e8497ab8a464aa500d99313737b4c5
ys634 <yutaro.sakairi@gmail.com>
2020-04-27 17:31:09 +0000

    themes

--- ui.R
+++ ui.R
@@ -1,6 +1,6 @@
-pkgs <- c('shiny', 'tidyverse','taxize', 'raster','plotly','reshape2', 'magrittr', 'ggplot2','ggridges','TrenchR','plotly', 'data.table', 'leaflet', 'shinyjs', 'maps','scales', 'shinysky')
-# 'pdftools', 'rgdal', 
-# lapply(pkgs, install.packages)
+pkgs <- c('shiny', 'tidyverse','taxize', 'raster','plotly','reshape2', 'magrittr', 'ggplot2','ggridges','TrenchR','plotly', 'data.table', 'leaflet', 'shinyjs', 'scales', 'shinysky', 'shinythemes', 'shinyWidgets')
+# 'pdftools', 'rgdal'
+
 lapply(pkgs, library, character.only = TRUE)
 
 #setwd("/Volumes/GoogleDrive/Shared Drives/TrEnCh/TSMVisualization/")
@@ -21,147 +21,156 @@ variables <- c("Month", "Hour", "Scenario", "Shade")
 lizards_tpc <- fread("lizards_tpc.csv")
 
 
-shinyUI <- fluidPage (
-  busyIndicator(text = "Data processing...", wait = 1000),
-  tags$head(tags$style(HTML('* {font-family: Calibri;}'))),
-  useShinyjs(),
-  titlePanel("Climate Change and Lizards"),
-  p("Short description of species added up to Sceloporus graciosus"),
-  hr(),
-  
-  h4("What is Thermal Safety Margin (TSM)?"),
-  
-  includeHTML("intro.html"),
-  br(),
-  column(12, align="center", plotOutput("intro_fig", width = "100%")),
-  
-  br(), 
-  
-  strong("Select a species and explore their distribution, current status and the risk they may face from increasing temperature."),
-  
-  fluidRow(
-    column(6, selectInput("species", label = "", choices = org_tpc, selected = "Psammodromus hispanicus"))
-  ),
-  
-  htmlOutput("species_info"),
-  
-  tabsetPanel(type = "tabs", 
-              tabPanel("Distribution Map",
-                       sidebarLayout(
-                         sidebarPanel(
-                           h3("Distribution map"),
-                           p("Set the variables and hit \"Run\" to take a look at the thermal safety margins of the selected species within their distribution."),
-                           p("Selecting \"Fraction\" will display the percentage of occurence of each TSMs over the entire distribution."),
-                           
-                           fluidRow(
-                             column(10, radioButtons("dist_frac", label = "Show", choices = c("Distribution", "Fraction"), selected = "Distribution"))
-                           ),
-                           hr(),
-                           fluidRow(
-                             column(6,radioButtons("rows", label = "Horizontal facets", choices = variables)),
-                             column(6,radioButtons("columns", label = "Vertical facets", choices = variables, selected = "Hour"))
-                           ),
-                           
-                           select2Input("month", label = "Month", choices = names(month), multiple = TRUE, selected = "January"),
-                           select2Input("hour", label = "Hour", choices = hours, multiple = TRUE, selected = "01 PM"),
-                           
-                           fluidRow(
-                             column(6, checkboxGroupInput("scenario", label = "Scenario", choices = scenarios, selected = "Normal")),
-                             column(6, checkboxGroupInput("shade", label = "Shade", choices = c("Exposed", "Covered"), selected = "Exposed"))
-                           ),  
-                           
-                           fluidRow(
-                             column(8, actionButton("map_onoff", "Show/Hide world map", styleclass = "success")),
-                             column(2, actionButton("run", "Run", styleclass = "primary"))
-                           ),
-                         ),
-                         
-                         mainPanel(
-                           conditionalPanel(
-                             condition = 'input.dist_frac == "Distribution"', fluidRow(column(12, align="center", plotOutput("plot1", width = "100%")))
-                           ),
-                           conditionalPanel(
-                             condition = 'input.dist_frac == "Fraction"', plotOutput("density")
-                           ),
-                           
-                           br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(),
-                           
-                           
-                             column(8, offset = 2, align="center", leafletOutput("mymap")),
-                           
-                           br(),
-                         )
-                       )
-              ),
-              
-              tabPanel("Thermal Performance Curve",
-                       sidebarLayout(
-                         sidebarPanel(
-                           h3("Thermal performance curve"),
-                           p("Take a look at the selected species' thermal performance curve and how varying environmental conditions affect their performance."),
-                           
-                           selectInput("month_tpc", label = "Month", choices = month, selected = 1),
-                           selectInput("hour_tpc", label = "Hour", choices = hours, selected = "01 PM"),
-                           
-                           fluidRow(
-                             column(6, radioButtons("scenario_tpc", label = "Scenario", choices = scenarios, selected = "Normal")),
-                             column(6, radioButtons("shade_tpc", label = "Shade", choices = c("Exposed", "Covered"), selected = "Exposed")),
-                           )
-                         ),
-                         
-                         mainPanel(
-                           br(), br(), br(), br(),
-                           plotOutput("TPC")
-                         )
-                       ),
-                       br(),
-              ),
-              
-              tabPanel("Plot", 
-                       sidebarLayout(
-                         sidebarPanel(
-                           h3("Plot"),
-                           p("Explore the change in thermal safety margins of the selected species throughout the day in different months."),
-                           p("The shapes on the plot represent the variation of TSM within the species due to the varying habitats of individuals."),
-                           
-                           radioButtons("facet", label = "Facet", choices = c("Shade", "Scenario"), inline = TRUE),
-                           
-                           selectInput("month_2", label = "Month", choices = month)
-                         ),
-                         
-                         mainPanel(
-                           column(12, align = "center", plotOutput("plot2", width = "100%"))
-                         )
-                       )
-              ),
-              
-              tabPanel("Raw Data", 
-                       sidebarLayout(
-                         sidebarPanel(
-                           h3("Raw Data"),
-                           p("Get the numric data on thermal safety margins here."),
-                           p("Select \"Annual\" and set TSM to 100 to see all our data."),
-                           
-                           hr(),
-                           
-                           h5("Show results for"),
-                           
-                           selectInput("month_data", label = "Month", choices = c(month, "Annual")),
-                           
-                           numericInput("value", label = "TSM Less than", value = 0),
-                           
-                           fluidRow(
-                             column(6, radioButtons("scenario_data", label = "Scenario", choices = scenarios, selected = "Normal")),
-                             column(6, radioButtons("shade_data", label = "Shade", choices = c("Exposed", "Covered"), selected = "Exposed")),
-                           )
-                         ),
-                         
-                         mainPanel(
-                           br(),
-                           h4("Mean thermal safety margins across the distibution"),
-                           htmlOutput("text_data")
-                         )
-                       )
-              )
-  )
+shinyUI <- fluidPage (theme = shinytheme("united"),
+                      busyIndicator(text = "Data processing...", wait = 1000),
+                      #tags$head(tags$style(HTML('* {font-family: Calibri;}'))),
+                      setBackgroundColor(color = "azure"),
+                      
+                      tags$head(
+                        tags$style(
+                          HTML("#dashboard{margin-left:500px;}")
+                        )
+                      ),
+                      useShinyjs(),
+                      
+                      
+                      titlePanel("Climate Change and Lizards"),
+                      p("Short description of species added up to Sceloporus graciosus"),
+                      hr(),
+                      
+                      h3("What is Thermal Safety Margin (TSM)?"),
+                      
+                      includeHTML("intro.html"),
+                      br(),
+                      column(12, align="center", plotOutput("intro_fig", width = "100%")),
+                      
+                      br(), 
+                      
+                      strong("Select a species and explore their distribution, current status and the risk they may face from increasing temperature."),
+                      
+                      fluidRow(
+                        column(6, selectInput("species", label = "", choices = org_tpc, selected = "Psammodromus hispanicus"))
+                      ),
+                      
+                      htmlOutput("species_info"),
+                      
+                      tabsetPanel(type = "tabs", 
+                                  tabPanel("Distribution Map",
+                                           sidebarLayout(
+                                             sidebarPanel(
+                                               h3("Distribution map"),
+                                               p("Set the variables and hit \"Run\" to take a look at the thermal safety margins of the selected species within their distribution."),
+                                               p("Selecting \"Fraction\" will display the percentage of occurence of each TSMs over the entire distribution."),
+                                               
+                                               fluidRow(
+                                                 column(10, radioButtons("dist_frac", label = "Show", choices = c("Distribution", "Fraction"), selected = "Distribution"))
+                                               ),
+                                               hr(),
+                                               fluidRow(
+                                                 column(6,radioButtons("rows", label = "Horizontal facets", choices = variables)),
+                                                 column(6,radioButtons("columns", label = "Vertical facets", choices = variables, selected = "Hour"))
+                                               ),
+                                               
+                                               select2Input("month", label = "Month", choices = names(month), multiple = TRUE, selected = "January"),
+                                               select2Input("hour", label = "Hour", choices = hours, multiple = TRUE, selected = "01 PM"),
+                                               
+                                               fluidRow(
+                                                 column(6, checkboxGroupInput("scenario", label = "Scenario", choices = scenarios, selected = "Normal")),
+                                                 column(6, checkboxGroupInput("shade", label = "Shade", choices = c("Exposed", "Covered"), selected = "Exposed"))
+                                               ),  
+                                               
+                                               fluidRow(
+                                                 column(8, actionButton("map_onoff", "Show/Hide world map", styleclass = "success")),
+                                                 column(2, actionButton("run", "Run", styleclass = "primary"))
+                                               ),
+                                             ),
+                                             
+                                             mainPanel(
+                                               conditionalPanel(
+                                                 condition = 'input.dist_frac == "Distribution"', fluidRow(column(12, align="center", plotOutput("plot1", width = "100%")))
+                                               ),
+                                               conditionalPanel(
+                                                 condition = 'input.dist_frac == "Fraction"', plotOutput("density")
+                                               ),
+                                               
+                                               br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(),
+                                               
+                                               
+                                               column(8, offset = 2, align="center", leafletOutput("mymap")),
+                                               
+                                               br(),
+                                             )
+                                           )
+                                  ),
+                                  
+                                  tabPanel("Thermal Performance Curve",
+                                           sidebarLayout(
+                                             sidebarPanel(
+                                               h3("Thermal performance curve"),
+                                               p("Take a look at the selected species' thermal performance curve and how varying environmental conditions affect their performance."),
+                                               
+                                               selectInput("month_tpc", label = "Month", choices = month, selected = 1),
+                                               selectInput("hour_tpc", label = "Hour", choices = hours, selected = "01 PM"),
+                                               
+                                               fluidRow(
+                                                 column(6, radioButtons("scenario_tpc", label = "Scenario", choices = scenarios, selected = "Normal")),
+                                                 column(6, radioButtons("shade_tpc", label = "Shade", choices = c("Exposed", "Covered"), selected = "Exposed")),
+                                               )
+                                             ),
+                                             
+                                             mainPanel(
+                                               br(), br(), br(), br(),
+                                               plotOutput("TPC")
+                                             )
+                                           ),
+                                           br(),
+                                  ),
+                                  
+                                  tabPanel("Plot", 
+                                           sidebarLayout(
+                                             sidebarPanel(
+                                               h3("Plot"),
+                                               p("Explore the change in thermal safety margins of the selected species throughout the day in different months."),
+                                               p("The shapes on the plot represent the variation of TSM within the species due to the varying habitats of individuals."),
+                                               
+                                               radioButtons("facet", label = "Facet", choices = c("Shade", "Scenario"), inline = TRUE),
+                                               
+                                               selectInput("month_2", label = "Month", choices = month)
+                                             ),
+                                             
+                                             mainPanel(
+                                               column(12, align = "center", plotOutput("plot2", width = "100%"))
+                                             )
+                                           )
+                                  ),
+                                  
+                                  tabPanel("Raw Data", 
+                                           sidebarLayout(
+                                             sidebarPanel(
+                                               h3("Raw Data"),
+                                               p("Get the numric data on thermal safety margins here."),
+                                               p("Select \"Annual\" and set TSM to 100 to see all our data."),
+                                               
+                                               hr(),
+                                               
+                                               h5("Show results for"),
+                                               
+                                               selectInput("month_data", label = "Month", choices = c(month, "Annual")),
+                                               
+                                               numericInput("value", label = "TSM Less than", value = 0),
+                                               
+                                               fluidRow(
+                                                 column(6, radioButtons("scenario_data", label = "Scenario", choices = scenarios, selected = "Normal")),
+                                                 column(6, radioButtons("shade_data", label = "Shade", choices = c("Exposed", "Covered"), selected = "Exposed")),
+                                               )
+                                             ),
+                                             
+                                             mainPanel(
+                                               br(),
+                                               h4("Mean thermal safety margins across the distibution"),
+                                               htmlOutput("text_data")
+                                             )
+                                           )
+                                  )
+                      )
 )
